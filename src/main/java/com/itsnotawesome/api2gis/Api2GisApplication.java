ccb2043603fd38c6fdc39e99419b37a2892da53a
Alexander <alexbary98@gmail.com>
2023-08-20 16:00:10 +0300

    Initial commit

---
+++ src/main/java/com/itsnotawesome/api2gis/Api2GisApplication.java
@@ -0,0 +1,13 @@
+package com.itsnotawesome.api2gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api2GisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api2GisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsnotawesome/api2gis/Api2GisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsnotawesome.api2gis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api2GisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
